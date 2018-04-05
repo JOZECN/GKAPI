3d6a09cb0e21f0290ff4f2a0fadedfaf177a3b05
周高爽 <1902348023@qq.com>
2018-04-05 20:51:07 +0800

    Jozecn
    按需求隔离端口

---
+++ index9004.js
@@ -0,0 +1,29 @@
+var express=require('express');
+var app=express();
+var path = require('path');
+var mongoose=require('mongoose');
+var bodyParser=require('body-parser');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use('/public',express.static(__dirname+'/public'));
+
+var swig=require('swig');
+app.engine('html',swig.renderFile);
+app.set('views','./views');
+app.set('view engine','html');
+swig.setDefaults({cache:false});
+
+app.use('/api',require('./routers/api'));
+app.use('/',require('./routers/main'));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/gkapi9004',function(err){
+    if(err){
+        console.log('数据库连接错误！');
+    }else{
+        console.log('数据库连接成功！');
+        app.listen(9001);
+    }
+});
